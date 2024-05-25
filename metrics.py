236b48bd6c9eb14257b784aa136a1dd42a961249
Andrey <126952251+Ozzzurk@users.noreply.github.com>
2024-05-25 15:19:00 +0300

    Add files via upload

--- metrics.py
+++ metrics.py
@@ -8,7 +8,7 @@ import datetime
 current_date = datetime.datetime.now()
 current_datetime = datetime.datetime.now().strftime("%d-%m-%y_%H-%M")
 
-
+# Настраиваем пути
 current_directory = os.path.dirname(os.path.abspath(__file__))
 if not os.path.exists(current_directory + "\\metrics"):
     os.makedirs(current_directory + "\\metrics")
@@ -18,10 +18,10 @@ new_directory_path = os.path.join(output_path, current_datetime)
 items = os.listdir(folder_path)
 
 
-# Отфильтруйте только файлы с расширением .csv
+# Оставляем только файлы с расширением .csv и начинающихся со слова "hardware"
 csv_files = [f for f in items if os.path.isfile(os.path.join(folder_path, f)) and f.lower().endswith('.csv') and f.lower().startswith('hardware')]
 
-# Проверка наличия файлов .csv
+# Работаем с файлом
 print("\n--------------------------------Проверка на наличие подходящих файлов-------------------------------")
 if csv_files:
     print(f"В текущей папке найдены файлы .csv:")
@@ -31,7 +31,6 @@ if csv_files:
 else:
     print(f"В текущей папке нет файлов .csv.")
     sys.exit("Программа завершена по причине отсутствия подходящих файлов для обработки.")
-
 
 while 1:
     n = int(input("\nВведите номер файла на чтение: "))
@@ -45,6 +44,7 @@ df = pd.read_csv(csv_files[n-1])
 print("\nВы загрузили файл: ", csv_files[n-1])
 columns = df.columns
 
+# Выбираем и отрисовываем графики
 print("\nДоступные графики:")
 numeric_columns = df.select_dtypes(include='number').columns.tolist()
 available_columns = {i+1: col for i, col in enumerate(numeric_columns)}
@@ -65,14 +65,10 @@ for index  in selected_indices:
         plt.title(column_name)
         plt.legend()
         
-        
         if not os.path.exists(new_directory_path):
             os.makedirs(new_directory_path)
-            #print(f"Директория {new_directory_path} создана успешно.")
-        
             
         save_path = os.path.join(new_directory_path, f"{column_name}_{current_datetime}.jpeg")
-        #print(save_path)
         plt.savefig(save_path, format='jpeg')
 
         plt.close()
